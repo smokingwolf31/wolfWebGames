95c41bdcc37afffebaa997ac292a0598e917a2fe
Mnqobi Nkabinde <nkbmnq002@sl-dual-261.cs.uct.ac.za>
2023-06-09 20:03:26 +0200

    Added the feature of altenating between pictures when playing pairs down

---
+++ src/tester.js
@@ -0,0 +1,9 @@
+
+
+let button1 = document.querySelector(".logoOn");
+const img = document.getElementById("less");
+console.log("Yes");
+button1.addEventListener("click", () => {
+    console.log("Yes");
+    img.src = "log.jpeg";
+})
